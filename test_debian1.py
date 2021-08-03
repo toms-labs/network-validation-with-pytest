aa161240128064b057422398f5b7d1a8f310cde6
Tom Ammon <thomasammon@gmail.com>
2021-08-03 00:27:04 -0500

    add bgp test

--- test_debian1.py
+++ test_debian1.py
@@ -1,3 +1,6 @@
+import json
+
+
 # we'll connect to devices and run commands a lot, so let's 
 # put this component into its own function right off the bat.
 def run_remote_command(host, cmd):
@@ -11,4 +14,10 @@ def test_file_contents(host):
     command = 'cat /opt/TomStatus.txt'
     filecontents = run_remote_command(host, command)
     assert filecontents.stdout.strip() == 'Tom is a goofball'
+
+def test_bgp_peer_up(host):
+    command = 'vtysh -c "show ip bgp summary json"'
+    output = run_remote_command(host, command)
+    bgp_data = json.loads(output.stdout)
+    assert bgp_data['ipv4Unicast']['peers']['192.168.1.2']['state'] == "Established"
 
